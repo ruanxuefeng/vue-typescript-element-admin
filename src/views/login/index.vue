cf36d594054dbec9c3e1489ccf749724371a4df4
ruanxuefeng <r19931229>
2020-04-25 18:41:10 +0800

    style(登录): 优化等页面代码

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -212,16 +212,16 @@
 
             input {
                 background: transparent;
-                border: 0px;
+                border: 0;
                 -webkit-appearance: none;
-                border-radius: 0px;
+                border-radius: 0;
                 padding: 12px 5px 12px 15px;
                 color: $light_gray;
                 height: 47px;
                 caret-color: $cursor;
 
                 &:-webkit-autofill {
-                    box-shadow: 0 0 0px 1000px $bg inset !important;
+                    box-shadow: 0 0 0 1000px $bg inset !important;
                     -webkit-text-fill-color: $cursor !important;
                 }
             }
@@ -282,7 +282,7 @@
             .title {
                 font-size: 26px;
                 color: $light_gray;
-                margin: 0px auto 40px auto;
+                margin: 0 auto 40px auto;
                 text-align: center;
                 font-weight: bold;
             }

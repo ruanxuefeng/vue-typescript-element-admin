8592e8db038ea011f149d26423d754923f9d1674
ruanxuefeng <r19931229>
2020-05-30 09:44:24 +0800

    feat: 添加访问外部页面组件

---
+++ src/components/Iframe/index.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="app-container" ref="appContainer">
+        <iframe ref="myIframe" :style="{height: iframeHeight}" @load="changeFrameHeight" v-bind:src="url" v-if="url"></iframe>
+    </div>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator';
+import RouteRecordImpl from '@/router/RouteRecordImpl';
+
+@Component({
+    components: {},
+})
+export default class Iframe extends Vue {
+    public $refs!: {
+        myIframe: HTMLElement,
+        appContainer: HTMLDivElement,
+    };
+    private url = '';
+    private iframeHeight = '800px';
+
+    private mounted () {
+        const {meta} = this.$route as RouteRecordImpl;
+        this.url = meta.url;
+        const appMainHeight = this.$refs.appContainer.parentElement?.offsetHeight ?? 1000;
+        this.iframeHeight = `${appMainHeight - 55}px`;
+        console.log(this.iframeHeight);
+    }
+}
+</script>
+
+<style scoped>
+    iframe {
+        border: none;
+        width: 100%;
+    }
+</style>
